89b650752aeeddfb48b366d9cef5a633b227923b
Sheila Nyambura <sheilanyambura394@gmail.com>
2023-10-27 15:45:48 +0300

    created an unordered list and added movie titles to it

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ fetch('db.json')
   .then((data) => {
     // Removing the movie titles and showing them
     const movieTitles = data.films.map((film) => film.title);
+
+    // Create an unordered list and add movie titles to it
+    const movieList = document.createElement('ul');
+    movieTitles.forEach((title) => {
+      const listItem = document.createElement('li');
+      listItem.textContent = title;
+      movieList.appendChild(listItem);
+    });
