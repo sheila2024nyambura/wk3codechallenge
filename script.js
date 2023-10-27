47ca723446c2c0b539deb227dc2cbfedbb974191
Sheila Nyambura <sheilanyambura394@gmail.com>
2023-10-27 15:43:25 +0300

    Removing movie titles

--- script.js
+++ script.js
@@ -2,4 +2,5 @@
 fetch('db.json')
   .then((response) => response.json())
   .then((data) => {
-    
+    // Removing the movie titles and showing them
+    const movieTitles = data.films.map((film) => film.title);
