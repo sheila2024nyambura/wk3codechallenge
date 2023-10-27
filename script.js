52fa74a35085ea8ab9ef3ae95265c4a8f9494d5c
Sheila Nyambura <sheilanyambura394@gmail.com>
2023-10-27 16:23:27 +0300

    Added different styles to the web page

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+//this file is accessed through  live server
 // Fetch the data from db.json
 fetch('db.json')
   .then((response) => response.json())
   .then((data) => {
-    // Removing the movie titles and showing them
+    // Extract the movie titles and display them
     const movieTitles = data.films.map((film) => film.title);
-
+    
     // Create an unordered list and add movie titles to it
     const movieList = document.createElement('ul');
     movieTitles.forEach((title) => {
